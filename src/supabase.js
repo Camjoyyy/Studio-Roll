7df1cd9691d861b018730ca26a4ee3fc473f0441
Camjoyyy <camerynjoyce@hotmail.com>
2026-06-08 15:39:39 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://suxtqzoqpwexyxpwnsld.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1eHRxem9xcHdleHl4cHduc2xkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQyMTYzODAsImV4cCI6MjAxOTc5MjM4MH0.NTBnfZn7iR5g_SoWSXzR7NJ_yxI0goT4xzzNmjf2ud0'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1eHRxem9xcHdleHl4cHduc2xkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4MTYxNzEsImV4cCI6MjA5NjM5MjE3MX0.NTBnfZn7iR5g_SoWSXzR7NJ_yxI0goT4xzzNmjf2ud0'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
